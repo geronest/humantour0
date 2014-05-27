08b9337bec2587504951d67fc07bd5a4aac5dc4b
SEONGHOSON <geronest@gmail.com>
2014-05-27 12:03:24 +0000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Humantour0::Application.config.secret_key_base = '1c20de1b49dbcec3e15b275422b72331c411bd223e41ac78ba32cac9ae00ce4e086dd387acadf20ad71a82ba2cd75907732a2b3be16a1cf2268de1198122ce5a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Humantour0::Application.config.session_store :cookie_store, key: '_humantour0_session'

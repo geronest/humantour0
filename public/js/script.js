1f17302832d87a049cd9eaca35cbba868af72227
SEONGHOSON <geronest@gmail.com>
2014-06-14 15:20:27 +0000

    about to fix link problem

--- public/js/script.js
+++ public/js/script.js
@@ -188,9 +188,12 @@ $( function() {
 		delay: 8000,
 
 		backgrounds: [
-			{ src: '../img/bg1.jpg', fade: 2000 },
 			{ src: '../img/bg2.jpg', fade: 2000 },
-			{ src: '../img/bg3.jpg', fade: 2000 }
+			{ src: '../img/bg3.jpg', fade: 2000 },
+			{ src: '../img/bg4.jpg', fade: 2000 },
+			{ src: '../img/bg5.jpg', fade: 2000 },
+			{ src: '../img/bg6.jpg', fade: 2000 },
+			{ src: '../img/bg7.jpg', fade: 2000 }
 		]
 	} );
 	
